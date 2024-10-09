cf00085bb43ec30f3183c9fc250065982088a4bd
R!turaj Gharat <rgharat1@asu.edu>
2024-10-08 23:15:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function generatePrompt() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer your_api_key_here'
+            'Authorization': 'Bearer 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'
         },
         body: JSON.stringify({ prompt: promptInput })
     })
